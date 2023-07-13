fb8a729a1131d26ee474b372ccc3aca1546938f4
bradenasmith2 <bradenasmith@icloud.com>
2023-07-13 12:13:54 -0500

    Commented out 'User_Input' property in States.cs to remove unhandled exception due to missing values in the 'User_Input' Column.

--- Tourism/Models/State.cs
+++ Tourism/Models/State.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Abbreviation { get; set; }
-        public string? UserInput { get; set; }
+        //public string? UserInput { get; set; }
     }
 }
